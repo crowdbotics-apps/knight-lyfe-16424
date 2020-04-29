b7d2f3d064b0a887fd3557218bcce0ab5ca80634
Crowdbotics <team@crowdbotics.com>
2020-04-29 23:53:50 +0000

    Initial commit.

---
+++ backend/knight_lyfe_16424/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for knight_lyfe_16424 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'knight_lyfe_16424.settings')
+
+application = get_wsgi_application()
